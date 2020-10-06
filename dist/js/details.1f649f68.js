9e3fa0ea0fa24d3f1d995ae881a0d402338dc3b8
lordkevinthebold <kevinbourassahoule@gmail.com>
2020-10-05 23:53:46 -0400

    init dist

---
+++ dist/js/details.1f649f68.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["details"],{"9f52":function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is the details page")])])}],i=n("2877"),l={},c=Object(i["a"])(l,s,a,!1,null,null,null);t["default"]=c.exports}}]);
+//# sourceMappingURL=details.1f649f68.js.map
---
+++ dist/precache-manifest.bd8c58b3e13e0ed760f74d537093753a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c34cdac275c919c6f22",
+    "url": "/css/app.3fca46ab.css"
+  },
+  {
+    "revision": "77c997367b6df326b5440f9e0b430cfd",
+    "url": "/img/aajoly-1.77c99736.jpg"
+  },
+  {
+    "revision": "a3e3040ef20c1dc8f729070ae9f82e32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c34cdac275c919c6f22",
+    "url": "/js/app.22775c5a.js"
+  },
+  {
+    "revision": "3e8dbe8d0afe5f1e271f",
+    "url": "/js/chunk-vendors.d0e98d75.js"
+  },
+  {
+    "revision": "35369300b50c98324477",
+    "url": "/js/details.1f649f68.js"
+  },
+  {
+    "revision": "8c8fa1ad586ddf734f89",
+    "url": "/js/location.7fa3a537.js"
+  },
+  {
+    "revision": "bf63523310f2c1355c35",
+    "url": "/js/schedule.bcbeb281.js"
+  },
+  {
+    "revision": "ea55bc36924323478e5c5b1cee6b5b0f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
